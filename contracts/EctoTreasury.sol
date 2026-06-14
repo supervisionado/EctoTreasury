4ca62861168cea97294887541370f8ee478599d2
SuperVisionado <lonshizong@gmail.com>
2026-06-14 17:33:10 -0300

    +Security / +Optimizations / +indexer friendly

--- contracts/EctoTreasury.sol
+++ contracts/EctoTreasury.sol
@@ -9,6 +9,19 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 using SafeERC20 for IERC20;
 
+/* 
+
+    Planned supported ERC-20 tokens on Ethereum blockchain:
+    USDT: 0xdAC17F958D2ee523a2206206994597C13D831ec7
+    USDC: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+
+    Planned supported ERC-20 tokens on Sonic blockchain:
+      USDC: 0x29219dd400f2Bf60E5a23d13Be72B486D4038894
+      USSD: 0x000000000eCcFf26B795F73fb0A70d48da657fEf  
+    PHANIC: 0xB4F58eeAbE36711D72e99f35cbf36210561522bE
+
+*/
+
 contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
 
     using Address for address payable;
@@ -18,22 +31,15 @@ contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
 
     event Deposit(
         address indexed user,
-        uint256 amount,
-        uint256 timestamp,
-        uint256 chainId
-    );
-
-    event TokenDeposit(
-        address indexed user,
         address indexed token,
-        uint256 amount,
-        uint256 timestamp,
-        uint256 chainId
+        uint256 amount
     );
 
     error InvalidDeposit();
+    error DepositFailed();
     error InsufficientBalance();
     error InvalidAddress();
+    error TokenNotAllowed();
 
     constructor() 
         Ownable(msg.sender) {
@@ -49,17 +55,15 @@ contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
     }
 
     // Deposit native ETH
-    function deposit() external payable nonReentrant {
+    function deposit() external payable nonReentrant whenNotPaused {
 
         if (msg.value<=0) revert InvalidDeposit();
 
         emit Deposit(
             msg.sender,
-            msg.value,
-            block.timestamp,
-            block.chainid
+            address(0),
+            msg.value
         );
-
 
     }
 
@@ -67,54 +71,56 @@ contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
     function depositToken(
         address token,
         uint256 amount
-    ) external
+    ) external nonReentrant whenNotPaused
     {
-        require(
-            allowedTokens[token],
-            "Token not allowed"
-        );
+        
+        if (!allowedTokens[token]) revert TokenNotAllowed();
+        if (amount==0) revert InvalidDeposit();
 
-        require(
-            amount > 0,
-            "Zero amount"
-        );
+        // Check token balance before the transfer
+        uint256 beforeBal = IERC20(token).balanceOf(address(this));
 
-        IERC20(token).transferFrom(
+        IERC20(token).safeTransferFrom(
             msg.sender,
             address(this),
             amount
-        );
+        );        
 
-        emit TokenDeposit(
-            msg.sender,
-            token,
-            amount,
-            block.timestamp,
-            block.chainid
-        );
+        // Get the delta: check token balance after the transfer, and substract
+        uint256 received = IERC20(token).balanceOf(address(this)) - beforeBal; 
+
+        emit Deposit(
+                       msg.sender,
+                       token,
+                       received
+                    ); 
+
+
     }    
 
     /// Allows the owner to withdraw some collected funds
-    function withdrawTo(
+    function withdrawEthTo(
         address to,
         uint256 amount
-    ) external nonReentrant onlyOwner {
+    ) external onlyOwner {
 
-        uint256 balance = address(this).balance;
-        if (amount >= balance) revert InsufficientBalance();
+        if (to == address(0)) revert InvalidAddress();
 
-        payable(to).sendValue(balance);
+        uint256 ethbalance = address(this).balance;
+        if (amount > ethbalance) revert InsufficientBalance();
+
+        payable(to).sendValue(amount);
     }
 
     /// Allows the owner to withdraw ALL collected funds 
-    function withdrawAllTo(address addTo) external nonReentrant onlyOwner {
+    function withdrawAllEthTo(address addTo) external onlyOwner {
 
-        if (addTo == address(0)) revert();        
-        uint256 balance = address(this).balance;
+        if (addTo == address(0)) revert InvalidAddress();   
 
-        if (balance==0) revert InsufficientBalance();
+        uint256 ethbalance = address(this).balance;
+        if (ethbalance==0) revert InsufficientBalance();
 
-        payable(addTo).sendValue(balance);
+        payable(addTo).sendValue(ethbalance);
 
     }     
 
@@ -145,16 +151,16 @@ contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
 
         if (to == address(0)) revert InvalidAddress();
 
-        uint256 balance =
+        uint256 tbalance =
             IERC20(token)
                 .balanceOf(address(this));
 
-        if (balance==0) revert InsufficientBalance();                
+        if (tbalance==0) revert InsufficientBalance();                
 
         IERC20(token)
             .safeTransfer(
                 to,
-                balance
+                tbalance
             );
 
     }
@@ -179,4 +185,6 @@ contract EctoTreasury is Ownable, Pausable, ReentrancyGuard {
         return IERC20(token)
             .balanceOf(address(this));
     }
+
+
 }
